07ffe4ed0b2d757cfd447da6c809203f63fdae15
PyLadiesDoboj <doboj@pyladies.com>
2019-06-01 22:46:36 +0200

    Python

---
+++ app.py
@@ -0,0 +1,12 @@
+﻿from flask import Flask
+
+pyladiesdo = Flask(__name__)
+
+
+@my_awesome_app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    pyladiesdo.run()
